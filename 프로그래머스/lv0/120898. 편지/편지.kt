584169ae372f7f769704e457b72546bf7762c9e4
phrabit <phrabit@gmail.com>
2023-09-13 09:33:01 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(message: String): Int {
+        var answer: Int = 0
+        
+        var len : Int = message.length
+        answer = len*2
+        
+        return answer
+    }
+}
