0dd968f64155f85d3daf497703fac3335c23ddd5
phrabit <phrabit@gmail.com>
2023-09-13 15:40:25 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.kt
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.kt
@@ -1,6 +1,9 @@
 class Solution {
     fun solution(arr: IntArray): Double {
         
+        // arr.average() 평균 바로 구하기
+        // arr.sum() 합계 구하기
+        
         var sum = 0
         val len = arr.size // 배열(array)의 길이 : .size
         
