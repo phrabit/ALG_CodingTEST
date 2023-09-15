9dcb07e44a1689e17e79300d07286d519d5d4f4a
phrabit <phrabit@gmail.com>
2023-09-15 22:23:49 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -4,7 +4,7 @@ def solution(A,B):
     new_A = sorted(A)
     new_B = sorted(B, reverse=True)
     
-    for i in range(len(A)):
-        answer += (new_A[i]*new_B[i])
+    for a,b in zip(new_A, new_B):
+        answer += (a*b)
 
     return answer
