fe595f05686169a6d1fe6d5a6f77bac2d3ab1a94
phrabit <phrabit@gmail.com>
2023-09-17 23:37:53 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,15 +1,42 @@
-def solution(s):
+# 나의 1차 풀이 -> 리스트로 풀기 실패
+# def solution(s):
+    
+#     change_cnt = 0 # 변환횟수
+#     zero_cnt = 0 # 제거된 0의 개수
+    
+#     #리스트로 풀자
+#     s_list = [i for i in s]
+    
+#     while True:
+#         if len(s_list) == 1:
+#             break;
+        
+#         for i in s_list:
+#             if i == '0':
+#                 s_list.remove(i)
+#                 zero_cnt+=1
+        
+#         tmp = bin(len(s_list))[2:]
+#         s_list = [i for i in tmp]
+#         change_cnt += 1
+    
+#     return [change_cnt, zero_cnt]
 
-    cnt = 0
-    zero_cnt = 0
+
+def solution(s):
+    change_cnt = 0 # 변환횟수
+    zero_cnt = 0 # 제거된 0의 개수
     
     while True:
-        zero_cnt += s.count('0')
-        s = s.count('1')*'1'
-        s = bin(len(s))[2:]
-        cnt += 1
         
-        if s=='1':
+        if len(s)==1:
             break;
+        
+        zero_cnt += s.count('0')
+        tmp = '1'*s.count('1')
+        s = bin(len(tmp))[2:]
+        change_cnt+=1
+        
     
-    return [cnt, zero_cnt]
+    return [change_cnt, zero_cnt]
+    
