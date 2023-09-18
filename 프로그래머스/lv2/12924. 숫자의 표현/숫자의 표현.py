20a382af9d38f3d4e4827b326ef91596eae9a1c2
phrabit <phrabit@gmail.com>
2023-09-18 14:33:57 +0900

    [level 2] Title: 숫자의 표현, Time: 5.36 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,15 +1,35 @@
-def solution(n):
+# 첫번째 풀이 : 시간초과
+# def solution(n):
+    
+#     li = [i for i in range(1, n+1)]
+    
+#     answer_li = []
+#     answer = 0
+    
+#     for i in range(2, len(li)):
+#         for j in range(n):
+#             if sum(li[j:j+i]) == n:
+#                 answer_li.append(li[j:j+i])
+    
+#     for i in answer_li:
+#         if len(i)!=1:
+#             answer += 1
+    
+#     return answer +1 
+
+            
+# 두번째 풀이
+# 내가 하려했던 로직은 같다... 단, 리스트로 구현하지 않았을 뿐.
+def solution(num):
     answer = 0
-    
-    for i in range(1, n+1):
+    for x in range(1,num+1):
         sum = 0
-        for j in range(i, n+1):
-            sum += j
-            if sum == n:
-                answer+=1
-                break;
-            elif sum>n:
-                break;
-    
-    
+        for y in range(x, num+1):
+            sum += y
+            if sum == num:
+                answer += 1
+                break
+            elif sum > num:
+                break
+
     return answer
