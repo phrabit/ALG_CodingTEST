0a7f5457d0e8485ec3feb266ef769c1c679ad0ac
phrabit <phrabit@gmail.com>
2023-09-19 22:06:08 +0900

    [level 2] Title: 피보나치 수, Time: 460.29 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,14 +1,27 @@
-#재귀호출 사용시, 시간초과 발생!!
-#리스트로 만들어서, 해결해야함.
-
+# 첫번째 풀이
+# 간단한 fibonacci 재귀함수로 구현 -> 시간초과 + 런타임에러
 def solution(n):
-    answer = []
-    for i in range(n+1):
-        if i==0 or i==1:
-            answer.append(i)
-        else:
-            f = answer[i-1] + answer[i-2]
-            answer.append(f % 1234567)
 
-    return answer[-1]
+    if n<2:
+        return n
+    else:
+        return solution(n-1) + solution(n-2)
+    
+# 두번째 풀이
+# stack 이용하여 풀기
+def solution(n):
+    li = [0,1]
+    
+    for _ in range(n):
+        li.append(li[-2]+li[-1])
+    
+    return li[n]%1234567
+
+print(solution(100))
+    
+    
+    
+    
+    
+    
     
