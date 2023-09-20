3b37396bd224311822e4c80508dd070a3ac25e7f
phrabit <phrabit@gmail.com>
2023-09-20 22:47:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 197.01 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,11 +1,10 @@
 def solution(s):
     stack = []
-    for i in s:
-        if len(stack)==0:
-            stack.append(i)
-        elif i == stack[-1]:
+    
+    for i in range(len(s)):
+        if len(stack)==0 or stack[-1] != s[i]:
+            stack.append(s[i])
+        else: # stack[-1] == s[i]
             stack.pop()
-        else:
-            stack.append(i)
 
-    return 1 if len(stack) == 0 else 0
+    return 1 if len(stack)==0 else 0
