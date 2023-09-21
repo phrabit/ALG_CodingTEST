6028fc2ea677d9c7f1b141026dafd3f1f0f38310
phrabit <phrabit@gmail.com>
2023-09-21 13:21:08 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,9 +1,14 @@
 def solution(brown, yellow):
     answer = []
     
-    tmp = brown+yellow
-    for i in range(1, tmp+1):
-        target = tmp//i
-        if brown == (i*2)+((target-2)*2):
-            if i>=target:
-                return [i, target]
+    sum = brown+yellow
+    
+    for i in range(sum-1, 1, -1):
+        if sum%i == 0:
+            j = sum//i
+            if j > i:
+                break;
+            elif (i-2)*(j-2) == yellow:
+                answer = [i,j]
+                
+    return answer
