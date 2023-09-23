416363976db4f02a78a00e3df4de0f007ac8a6dc
phrabit <phrabit@gmail.com>
2023-09-23 21:04:04 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,10 @@
 def solution(n):
-    answer = 0
-    tmp = n
-
+    # 매개변수로 주어진 n의 2진수 변환 시 1의 개수를 변수로 저장
+    target = bin(n)[2:].count('1') 
+    
     while True:
-        tmp += 1
+        n += 1 # 1씩 증가시키면서 체크
         
-        if bin(tmp)[2:].count('1') == bin(n)[2:].count('1') :
-            return tmp
+        # 만약 증가된 n의 2진수 변환시 1의 개수가 target과 같다면 그 수 return
+        if bin(n)[2:].count('1') == target:
+            return n
