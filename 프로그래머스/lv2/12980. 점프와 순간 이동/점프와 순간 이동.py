9458d53cc5dfe9b24afeacbd97dd2f977b61543f
phrabit <phrabit@gmail.com>
2023-09-26 00:18:30 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,25 +1,11 @@
-# <나의 코드>
-# 한가지 경우에 대해서만 시간초과 나고, 나머진 다 맞음 (96/100)
-
-# def solution(n):
-#     ans = 0
+def solution(n):
+    battery_usage = 0
     
-#     while n>1:
-#         if n%2==0:
-#             n = n//2
-#         else:
-#             n -= 1
-#             n = n//2
-#             ans += 1   
-
-#     return ans+1
-
-
-
-# <정답 코드>
-def solution(N):
-    answer = 0
-    while N > 0:
-        answer += N % 2
-        N //= 2
-    return answer
+    while n:
+        if n%2==0:
+            n//=2
+        else:
+            n-=1
+            battery_usage += 1
+    
+    return battery_usage
