3c7169edd338d87d586c572667fbd2d2e5a2762e
phrabit <phrabit@gmail.com>
2023-09-26 00:00:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,57 +1,10 @@
-# 내 풀이는, 모든 경우의 수를 다 나눴음.
-# def solution(n,a,b):
-#     cnt = 0
-    
-#     while True:
-#         li = []
-#         if a%2==0 and b%2==0:
-#             a= a//2
-#             b= b//2
-#             li = sorted([a,b])
-#             if li[1]-li[0] == 1:
-#                 cnt+=1
-#                 break;
-#             else:
-#                 cnt+=1
-#         elif a%2!=0 and b%2!=0:
-#             a = (a+1)//2
-#             b = (b+1)//2
-#             li = sorted([a,b])
-#             if li[1]-li[0] == 1:
-#                 cnt+=1
-#                 break;
-#             else:
-#                 cnt+=1
-#         elif a%2!=0 and b%2==0:
-#             a = (a+1)//2
-#             b= b//2
-#             li = sorted([a,b])
-#             if li[1]-li[0] == 1:
-#                 cnt+=1
-#                 break;
-#             elif li[0]-li[1] == 0:
-#                 break;
-#             else:
-#                 cnt+=1
-#         elif a%2==0 and b%2!=0:
-#             b= (b+1)//2
-#             a = a//2
-#             li = sorted([a,b])
-#             if li[1]-li[0] == 1:
-#                 cnt+=1
-#                 break;
-#             elif li[0]-li[1] == 0:
-#                 break;
-#             else:
-#                 cnt+=1
-
-#     return cnt+1
-
-def solution(n,a,b): 
-	answer = 0
-	while a != b: 
-		answer += 1
-        # 1을 더해서 2로 나누었을 때, 자리수를 맞춰줌
-        # 예) 1, 2의 경우는 2, 3으로 해서 나눴을때 몫이 1이 되도록
-		a, b = (a+1)//2, (b+1)//2
-	return answer
+def solution(n,a,b):
+    cnt = 1
+    while True:
+        a = (a//2) + (a%2)
+        b = (b//2) + (b%2)
+        if a == b:
+            break
+        cnt += 1
+        
+    return cnt
