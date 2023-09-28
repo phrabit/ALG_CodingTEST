14a9bdebb72650d565c06487575cae4a569a2b51
phrabit <phrabit@gmail.com>
2023-09-28 22:30:01 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,10 +1,14 @@
 def solution(n,a,b):
     cnt = 1
-    while True:
+    
+    while True:            
         a = (a//2) + (a%2)
         b = (b//2) + (b%2)
-        if a == b:
-            break
-        cnt += 1
         
+        if a==b:
+            break;
+        
+        cnt+=1
+    
     return cnt
+        
