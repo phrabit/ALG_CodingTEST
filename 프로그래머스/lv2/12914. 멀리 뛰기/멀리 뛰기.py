be7c669360a950e67d73391fd1e6f3a03443da9f
phrabit <phrabit@gmail.com>
2023-09-30 18:05:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.39 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,32 +1,16 @@
-from itertools import combinations        
-
-
-# 첫번째 풀이 : 12.5/100 -> 시간초과
-def solution(n):
-    cnt = 1 
-    
-    for num2 in range(1, n//2+1):
-        num1 = (n-2*num2)
-        
-        if num1 > num2 :
-            li = ['0'] * (num1+1)
-            cnt += len(list(combinations(li, num2)))
-        else:
-            li = ['0'] * (num2+1)
-            cnt += len(list(combinations(li,num1)))
-
-    return cnt
-
-
-# 두번째 풀이 : DP + 점화식(피보나치)
 def solution(n):
     if n<3:
         return n
     
     dp = [0] * (n+1)
-    dp[1]=1
-    dp[2]=2
-    for i in range(3,n+1):
-        dp[i]=dp[i-1]+dp[i-2]
+    
+    dp[1] = 1
+    dp[2] = 2
+    
+    
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        
     return dp[n]%1234567
+    
     
