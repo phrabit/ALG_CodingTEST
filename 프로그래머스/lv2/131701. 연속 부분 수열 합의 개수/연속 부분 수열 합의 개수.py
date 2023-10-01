95a67c30d4ef47cc239d4e9f3a217c78cef38084
phrabit <phrabit@gmail.com>
2023-10-02 01:51:43 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 7872.96 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,5 +1,5 @@
 # 나의 1차 풀이 -> 완전탐색(brute force)
-# : 테스트3부터 시간초과 발생...
+# 시간초과 발생
 
 def solution(elements):
     
@@ -12,12 +12,20 @@ def solution(elements):
         for j in range(1, len(elements)+1):
             tmp = sum(db_elements[i:i+j])
 
+            # <이 부분이 시간초과의 원인>
             if tmp not in sum_li:
                 sum_li.append(tmp)
     
     return len(sum_li)
+
+
+
+
+
         
-    
+# 나의 두번째 풀이
+# : 리스트 길이를 늘린 동일 방법이지만, 이중 포문 안에 한번도 sum_li를 순회하는 코드 제거함.
+# : 시간 초과 해결
 def solution(elements):
     sum_li = set()
 
@@ -27,3 +35,27 @@ def solution(elements):
         for j in range(0, len(elements)):
             sum_li.add(sum(db_elements[j:j+i+1]))
     return len(sum_li)
+
+
+
+
+
+
+
+
+# 다른 사람 풀이
+# 회전 큐(circular Queue)를 사용한 풀이
+# 매 회전 마다 부분 합을 따로 구하게 되면, 결국 전체 부분합 경우를 모두 고려할 수 있다.
+# deque.rotate(1) == list[i:] + list[:i]
+
+from collections import deque
+ 
+def solution(elements):
+    answer = set()
+    elements = deque(elements)
+    for j in range(len(elements)):
+        elements.rotate(1)
+        for i in range(1,len(elements)):
+            answer.add(sum(list(elements)[:i]))
+    answer.add(sum(elements))
+    return len(answer)
