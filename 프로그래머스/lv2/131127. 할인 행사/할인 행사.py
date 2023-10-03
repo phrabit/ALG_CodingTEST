8495772414f9831c506d0feb6c8c569770150977
phrabit <phrabit@gmail.com>
2023-10-03 12:16:22 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,9 +1,12 @@
 from collections import Counter
-# discount정보를 Counter로..!
-# market = Counter(discount)
-    
-    
-    
+
+# 나의 풀이 (100/100)
+
+# 풀이방법
+# : want와 number를 딕셔너리 형태로 묶어줌. 그러면 품목과 개수 형태가 묶여서 나옴.
+# : discount 리스트를 인덱스 0부터 10개의 항목씩 봐야 하므로 for문 조정.
+# : my_want와 market을 비교하여 같으면 cnt를 올려줌
+# : 비교하기 위해 두 딕셔너리를 key값으로 정렬시킴. (좋은 방법인진 모르겠다)
 
 def solution(want, number, discount):
     
