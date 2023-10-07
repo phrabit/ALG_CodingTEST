9637a14fec7b67c8ddd84c09dcf9c55687c78a79
phrabit <phrabit@gmail.com>
2023-10-08 08:43:03 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.59 ms, Memory: 28.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -4,9 +4,26 @@ def solution(arr1, arr2):
     result = np.dot(arr1, arr2)
     
     # ndarray 형태를 파이썬 list형태로 바꿔주기
+    # 안그러면 TypeError: Object of type int64 is not JSON serializable 에러남.
     result = result.tolist()
     
     # print(answer)
     return result
     
+    
+# 내적 구현하는 정석 풀이
+def solution(arr1, arr2):
+    
+    # 배열 초기화
+    answer = [[0]*len(arr2[0]) for _ in range(len(arr1))]
+    
+    
+    for i in range(len(arr1)): 
+        lists = []
+        for j in range(len(arr2[0])): 
+            for k in range(len(arr1[0])): 
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+                
+                
+    return answer
     
