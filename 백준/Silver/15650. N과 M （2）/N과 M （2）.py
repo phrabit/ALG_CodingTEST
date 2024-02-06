738bb23a56547fe1ea8446912a33b8d6ab2b6fbc
phrabit <phrabit@gmail.com>
2024-02-06 12:11:15 +0900

    [Silver III] Title: N과 M (2), Time: 152 ms, Memory: 111264 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,21 @@
+def DFS(L):
+    if L == m:
+        if res == sorted(res):
+            for i in res:
+                print(i, end=" ")
+            print()
+    else:
+        for i in range(1, n+1):
+            if check[i] == 0:
+                check[i] = 1
+                res[L] = i
+                DFS(L+1)
+                check[i] = 0
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+
+    check = [0]*(n+1)
+    res = [0]*m
+
+    DFS(0)
