c97485d7e1b721c6b4a66b11a3cafd10a9752966
phrabit <phrabit@gmail.com>
2024-03-07 08:11:00 +0900

    [Gold V] Title: 평범한 배낭, Time: 160 ms, Memory: 110024 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,8 @@
+if __name__=="__main__":
+    n, m=map(int, input().split())
+    dy=[0]*(m+1);
+    for i in range(n):
+        w, v=map(int, input().split())
+        for j in range(m, w-1, -1):
+            dy[j]=max(dy[j], dy[j-w]+v)
+    print(dy[m])
